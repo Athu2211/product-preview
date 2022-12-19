3958a3a8acd50a2e3a3dce0ccf4c1e2ad4a8b88b
nxf87957 <athulya.arokiyasamy@nxp.com>
2022-12-19 18:19:51 +0530

    Product-preview-card-component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { Row, Col, Button, Container } from 'react-bootstrap'
+import productImage from './images/image-product-desktop.jpg';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className='content'>
+      <Row>
+        <Col xs={5} className='product-image-outer'>
+          <img className='product-image' alt='product' src={productImage} />
+        </Col>
+        <Col xs={6} className="product-details">
+          <h5 className='product'>PERFUME</h5>
+          <h1 className='product-name'>Gabrielle Essence Eau De Parfum</h1>
+          <p className='product-description'>A floral, solar and voluptuous interpretation composed by Olivier Polge,
+            Perfumer-Creator for the House of CHANEL.</p>
+          <span className='cost'>$149.99</span>&nbsp;&nbsp;&nbsp;<span className='old-cost'>$169.99</span>
+          <div className='btn'>
+            <Button className='add-to-cart'><span className='fa fa-cart fa-shopping-cart'></span> Add to Cart</Button>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
